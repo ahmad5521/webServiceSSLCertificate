5d50a400ac1542e72c9b653120cef6017c6f8e4a
ahmasiri <ahmasiri@N16387>
2019-05-20 14:54:19 +0300

    initial commit

---
+++ consumeWebServiceUsingSSLCertificate/consumeWebServiceUsingSSLCertificate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace consumeWebServiceUsingSSLCertificate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
